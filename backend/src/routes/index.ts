834bed6fbfe70a03e2efe0d48a307dcd86b4cae4
adamberri <adamberri123@gmail.com>
2023-01-18 18:26:43 -0500

    Added the new route with a 400 error if the department doesnt work

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,16 +2,59 @@ import { Router, Response, Request } from 'express'
 
 const router: Router = Router()
 
-
 router.get('/v1/departments/:id', (_req: Request, res: Response) => {
-    console.log(_req.params)
-    res.send([
-        {
-            id: 2,
-            name: "Full Stack Development",
-            employeeIds: ["82338", "32673"]
-        },
-    ])
+    const department_id = _req.params.id
+    if (department_id == "2") {
+        res.send([
+            {
+                id: 82338,
+                name: "Molly Davis",
+                birthday: new Date("1985-09-27"),
+                bio: "Molly once found a bug in a compiler",
+                departmentId: "2"
+            },
+            {
+                id: "32673",
+                name: "François Allende",
+                birthday: new Date("1985-09-27"),
+                bio: "François is the best QA engineer West of the Susquehanna river.",
+                departmentId: "2"
+            }
+        ])
+    }
+    if (department_id == "3") {
+        res.send([
+            {
+                id: "82837",
+                name: "Bob Smith",
+                birthday: new Date("1985-09-27"),
+                bio: "Bob has been programming computers for entirely too long!",
+                departmentId: "3"
+            },
+        ])
+    }
+    if (department_id == "4") {
+        res.send([
+            {
+                id: "81832",
+                name: "Ada Burr",
+                birthday: new Date("1985-10-18"),
+                bio: "Ada loves full stack development.",
+                departmentId: "4"
+            },
+            {
+                id: "zc",
+                name: "Juan Cortez",
+                birthday: '1984-09-22',
+                bio: "Juan's been programming computers since the days of ATARI BASIC.",
+                departmentId: "4"
+            }
+        ])
+    }
+    //raise error
+    res.sendStatus(400)
+    res.render('error', { error: "This Department ID Does Not Exist" })
+
 })
 
 
@@ -53,7 +96,7 @@ router.get('/v1/employees', (_unused: Request, res: Response) => {
             departmentId: "4"
         },
         {
-            id: 82338,
+            id: "82338",
             name: "Molly Davis",
             birthday: new Date("1985-09-27"),
             bio: "Molly once found a bug in a compiler",
